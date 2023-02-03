9397b61611aa1c9ed022d0408f5056f8bdce972c
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-03 13:36:16 +0300

    Added spawn points

---
+++ Assets/Script/Data/Constants.cs
@@ -0,0 +1,7 @@
+namespace Script.Data
+{
+    public class Constants
+    {
+        public const string SpawnPoint = "SpawnPoint";
+    }
+}
---
+++ Assets/Script/Data/LevelStaticData.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+using Cinemachine;
+using UnityEngine;
+
+namespace Script.Data
+{
+    public class LevelStaticData : MonoBehaviour
+    {
+        public Transform[] SpawnPoints;
+        public Camera MainCamera;
+        public CinemachineVirtualCamera ThirdPersonCamera;
+
+        private void Reset()
+        {
+            GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag(Constants.SpawnPoint);
+            SpawnPoints = spawnPoints.Select(go => go.transform).ToArray();
+        }
+    }
+}
