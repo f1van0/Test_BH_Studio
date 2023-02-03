17f67f88f62bc567b25dd0787b6fc61d8e0ae6de
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-03 13:50:54 +0300

    Added input handler and container that contain each part. of the character

---
+++ Assets/Script/Game/Character/CharacterInputHandler.cs
@@ -0,0 +1,50 @@
+using Script.Game.Character.Ability;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Script.Game.Character
+{
+    public class CharacterInputHandler : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput input;
+        [SerializeField] private CharacterMovement movement;
+        [SerializeField] private CameraController cameraController;
+        [SerializeField] private ChargeAbility chargeAbility;
+        [SerializeField] private Transform modelTransform;
+
+        private InputAction _walkAction;
+        private InputAction _chargeAction;
+        private InputAction _lookAction;
+
+        private void Awake()
+        {
+            _walkAction = input.actions["Walk"];
+            _chargeAction = input.actions["Charge"];
+            _lookAction = input.actions["Look"];
+            _chargeAction.performed += OnChargePerformed;
+            chargeAbility.Completed += OnChargeCompleted;
+        }
+
+
+        private void FixedUpdate()
+        {
+            var walk = _walkAction.ReadValue<Vector2>();
+            if (!chargeAbility.IsCharging)
+                movement.Move(walk);
+
+            cameraController.RotateCamera(_lookAction.ReadValue<Vector2>());
+        }
+
+        private void OnChargePerformed(InputAction.CallbackContext _)
+        {
+            if (chargeAbility.IsCharging) return;
+
+            chargeAbility.TriggerCharge(modelTransform.forward);
+        }
+
+        private void OnChargeCompleted(ChargeAbility _)
+        {
+            Debug.Log("Charge completed");
+        }
+    }
+}
